c47410b9e842ca1a954abef6e40f9f32afd0e895
IsmoilovMax <maxmudjon2121@gmail.com>
2024-05-15 00:46:24 +0900

    EJS framework orqali ananaviy fronted qurish

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 console.log('Web Serverni boshlash');
 //npm i express (ustanovka)
 const express = require("express");
@@ -15,18 +16,21 @@ const http = require("http");
 //2: Session code
 
 
-//3 Views code: fronenddi beken ichida yasaydigan views ejs(npm i ejs)
+//3 Views code: fronenddi beken ichida yasaydigan views ejs(npm i ejs) bsr
 app.set("views", "views"); // folderdi ozgaritmoqchi bolsak views di ozgartiramiz
 app.set("view engine", "ejs") // view folderdan oqidigon
 
-//4: Routing code: Routerlarga moljalangan . 
-app.get("/hello", function(req, res) {
-    res.end(`<h1 style="background: red">Hello world Max</h1>`);
-});
-app.get("/gift", function(req, res) {
-    res.end(`<h1 style="background: blue">Siz sovgalar bolimidasiz</h1>`);
-});
+//4: Routing code: Routerlarga moljalangan 
+//
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"}); //json shakldagi malumotdi qaytaradi
 
+})
+ //malumotdi date basedan olis uchun
+app.get("/", function(req, res){
+    res.render("harid");
+})
 //http bizning core modulimiz
 //createServer(param) serverdi yasash uchun . parametr qabul qiladi
 const server = http.createServer(app);
@@ -37,3 +41,17 @@ server.listen(PORT, function() {
 });
 //npm start
 //npm i nodemon bundan keyin (npm run dev) ishlatish kere
+//.gitignore=.+node_modules
+//git init
+//git status
+//git add .
+//git status
+//git commit -m "BRR:mavzu nomi"
+//git log --oneline
+//git status
+//git branch
+//git remote
+//git push orign masters
+//function - async -sync
+
+
---
+++ train.js
@@ -0,0 +1,9 @@
+console.log("Train area")
+
+//B- TASK
+
+
+
+//A-TASK
+
+
