f0a285efb6c8735b9e69583e2b9a6c14f5cd7b3f
IsmoilovMax <maxmudjon2121@gmail.com>
2024-05-30 17:44:00 +0900

    30-32 MAVZA TASK-E

--- train.js
+++ train.js
@@ -1,16 +1,14 @@
-/*D-TASK: 
+/*
+E-TASK: 
 
-Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
-MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
+MASALAN: getReverse("hello") return qilsin "olleh"
 */
 
-function checkContent(str1, str2) {
-    // Har ikkala stringni harflar bo'yicha saralash va solishtirish
-    return str1.split('').sort().join('') === str2.split('').sort().join('');
-  }
-  
-  // Test
-  console.log(checkContent("mitgroup", "gmtiprou")); // true
-  console.log(checkContent("example", "elpmaxe")); // true
-  console.log(checkContent("hello", "world")); // false
-  
+function getReverse(str) {
+  return str.split('').reduce((reversed, char) => char + reversed, '');
+}
+
+// Misol uchun:
+let result = getReverse("ISMOILOV");
+console.log(result);  // Natija: "olleh"
